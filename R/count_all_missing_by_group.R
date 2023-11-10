73d06c7036c3a145a14b4b3b41fd891d72904688
Zoe Zhang <jane@example.com>
2023-11-10 13:46:23 -0800

    update

--- R/count_all_missing_by_group.R
+++ R/count_all_missing_by_group.R
@@ -1,5 +1,17 @@
 #' Count missing values for all columns by group
 #'
+#' @description Given a data frame `data` and a
+#' column `group`, `count_all_missing_by_group()` creates
+#' a new data frame with one row per level of `group`.
+#' The first column of the output data frame
+#' contains the levels of `group`, and the rest of
+#' the columns contain the number of missing values for
+#' all columns in `data` except `group`.
+#'
+#' @details This function is
+#' a wrapper around the `group_by()` and
+#' `summarize()` functions in the `dplyr` package.
+#'
 #' @param data A data frame or tibble to process.
 #' @param group_col Column in `data` to group by
 #' @param .groups (OPTIONAL) Controls whether the
@@ -13,12 +25,13 @@
 #' levels of `group`, and the  number of NAs within
 #' each level of `group` for all columns in
 #' `data` except `group`.
-#' @export
 #'
 #' @examples
 #' library(palmerpenguins)
 #' count_all_missing_by_group(airquality, Month)
 #' @importFrom dplyr summarize group_by across everything
+#' @export
+#'
 count_all_missing_by_group <- function(data, group_col, .groups = "drop") {
 
   if(!is.null(.groups)) {
