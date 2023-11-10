60d50718658edc3f6b85891ceb4142e07890a487
Zoe Zhang <jane@example.com>
2023-11-09 19:05:23 -0800

    update

--- countNAs/R/count_all_missing_by_group.R
+++ countNAs/R/count_all_missing_by_group.R
@@ -1,1 +1,35 @@
-# This is a function to count missing values for all columns by group
+#' Count missing values for all columns by group
+#'
+#' @param Data in data.frame or tibble format
+#' @param group_col Column in `data` to group by
+#' @param .groups (OPTIONAL) Controls whether the
+#' output data frame is grouped. By default,
+#' drops all levels of grouping (`"drop"`). Specify
+#' `"keep"` to keep the output data frame grouped by
+#' `group`. See `summarize()` documentation in
+#' `dplyr` for a full list of options.
+#'
+#' @return A data frame or tibble with the
+#' levels of `group`, and the  number of NAs within
+#' each level of `group` for all columns in
+#' `data` except `group`.
+#' @export
+#'
+#' @examples
+#' library(palmerpenguins)
+#' count_all_missing_by_group(airquality, Month)
+count_all_missing_by_group <- function(data, group_col, .groups = "drop") {
+
+  if(!is.null(.groups)) {
+    if(!(.groups %in% c("drop_last", "drop",
+                        "keep", "rowwise"))) {
+      stop(".groups needs to be one of NULL,
+         \"drop_last\", \"drop\", \"keep\",
+         and \"rowwise\".")
+    }
+  }
+
+  data |> group_by({{ group_col }}) |>
+    summarize(across(everything(), ~sum(is.na(.x))),
+              .groups = .groups)
+}
