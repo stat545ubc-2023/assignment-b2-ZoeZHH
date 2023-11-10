3dab6dcec2c9c83aede6edda1412bcec1a0c3418
Zoe Zhang <jane@example.com>
2023-11-10 14:04:50 -0800

    update

--- tests/testthat/test-count_all_missing_by_group.R
+++ tests/testthat/test-count_all_missing_by_group.R
@@ -1,20 +1,19 @@
-library(tibble)
 test_that("Output matches direct call to dplyr", {
-  small_tbl <- tribble(~group, ~var1, ~var2,
-                       "A", 1, NA,
-                       "A", 2, "x",
-                       "B", NA, "y",
-                       "C", 3, "z")
+  small_tbl <- tibble::tribble(~group, ~var1, ~var2,
+                               "A", 1, NA,
+                               "A", 2, "x",
+                               "B", NA, "y",
+                               "C", 3, "z")
 
-  expect_equal( small_tbl |> group_by(group) |>
-                  summarize(across(everything(), ~sum(is.na(.x))),
-                            .groups = "drop"),
+  expect_equal( small_tbl |> dplyr::group_by(group) |>
+                  dplyr::summarize(dplyr::across(dplyr::everything(), ~sum(is.na(.x))),
+                                   .groups = "drop"),
                 count_all_missing_by_group(small_tbl, group)
   )
 
-  expect_equal( small_tbl |> group_by(group) |>
-                  summarize(across(everything(), ~sum(is.na(.x))),
-                            .groups = NULL),
+  expect_equal( small_tbl |> dplyr::group_by(group) |>
+                  dplyr::summarize(dplyr::across(dplyr::everything(), ~sum(is.na(.x))),
+                                   .groups = NULL),
                 count_all_missing_by_group(small_tbl, group, NULL)
   )
 })
